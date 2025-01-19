6d91713306f4740a5cd977640629344561bccd1b
Nicolas A. Serna <serna.nicolas.abel@gmail.com>
2025-01-19 03:01:47 -0300

    update tests for constants

--- calculator_test.go
+++ calculator_test.go
@@ -1,6 +1,8 @@
 package calculator_test
 
 import (
+	"fmt"
+	"math"
 	"testing"
 
 	"github.com/nicolito128/go-calculator"
@@ -29,6 +31,113 @@ func TestResolveParenthesis(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestResolveImplicitMult(t *testing.T) {
+	{
+		got, err := calculator.Resolve("(3)(4)")
+		want := float64(12)
+
+		if err != nil {
+			t.Errorf("error occurred: %v", err)
+		}
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	{
+		got, err := calculator.Resolve("(5)(5)2")
+		want := float64(50)
+
+		if err != nil {
+			t.Errorf("error occurred: %v", err)
+		}
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+}
+
+func TestMathConstants(t *testing.T) {
+	{
+		got, err := calculator.Resolve("e")
+		want := fmt.Sprintf("%f", math.E)
+
+		if err != nil {
+			t.Errorf("error occurred: %v", err)
+		}
+
+		s := fmt.Sprintf("%f", got)
+		if s != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	{
+		got, err := calculator.Resolve("pi")
+		want := fmt.Sprintf("%f", math.Pi)
+
+		if err != nil {
+			t.Errorf("error occurred: %v", err)
+		}
+
+		s := fmt.Sprintf("%f", got)
+		if s != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	{
+		got, err := calculator.Resolve("phi")
+		want := fmt.Sprintf("%f", math.Phi)
+
+		if err != nil {
+			t.Errorf("error occurred: %v", err)
+		}
+
+		s := fmt.Sprintf("%f", got)
+		if s != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	{
+		got, err := calculator.Resolve("ln10")
+		want := fmt.Sprintf("%f", math.Ln10)
+
+		if err != nil {
+			t.Errorf("error occurred: %v", err)
+		}
+
+		s := fmt.Sprintf("%f", got)
+		if s != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	{
+		got, err := calculator.Resolve("ln2")
+		want := fmt.Sprintf("%f", math.Ln2)
+
+		if err != nil {
+			t.Errorf("error occurred: %v", err)
+		}
+
+		s := fmt.Sprintf("%f", got)
+		if s != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	{
+		got, err := calculator.Resolve("pi/2 + (3)(4) - e")
+		want := 10.852514498335852
+
+		if err != nil {
+			t.Errorf("error occurred: %v", err)
+		}
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
 	}
 }
 
